b8f203696ed8ec912a8fbd30091c5167e7cc71e2
KarlaIvon15 <230094@utxicotepec.edu.mx>
2024-08-07 20:49:26 -0600

    Add files via upload
    cambios

---
+++ apuntes.js
@@ -0,0 +1,52 @@
+// Comentarios
+
+alert('Hola desde un alert');
+
+// var apellido = "Vargas"; (variable)
+let nombre = "Karla";
+
+const pi= 3.14;
+// string nombre="Juan";
+// Tipos de datos
+// No definido
+let noDefinido;
+// Número
+let cantidad = 50;
+// Cadena de texto o String
+let bebida = "agua";
+// booleanos
+let activo = true;
+
+// Objetos
+// Persona: nombre, apellido, edad, peso
+let persona = {nombre :'Karla', apellido : 'Vargas', edad : 19,};
+console.log(persona);
+// Arrays coleccion
+let personas = [
+    {nombre: 'Erandy', apellido : 'Mendoza', edad : 19},
+    {nombre: 'Guadalupe', apellido : 'Saavedra', edad : 18},
+    {nombre: 'Avryl', apellido : 'Salas', edad : 20,},
+    persona
+]
+console.log(personas);
+console.log(personas [2])
+
+// Funciones JS
+// Listado  de instrucciones
+function suma(params){
+    return 2+2;
+}
+let resultado = suma();
+console.log(resultado);
+// Funciones JS
+// Listado de instrucciones
+function suma(a, b) {
+    return a + b;
+}
+let resultadoDeSuma = suma(9, 7);
+console.log(resultadoDeSuma);
+function multiplicacion(a , b){
+ return a * b;
+}
+let resultado = multiplicacion(5,7);
+console.log(resultado);
